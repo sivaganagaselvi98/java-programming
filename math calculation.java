c6b60b82bce667644ea65bfe140e08104a9a31ba
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-07 11:04:00 +0530

    Add files via upload

---
+++ math calculation.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a = 15;b = 6;
+    c = a-b;
+    d = a+b;
+    e = a*b;
+    f = a/b;
+    System.out.println("the subtraction of two numbers"+c);
+    System.out.println("the addition of two numbers"+d);
+    System.out.println("the product of two numbers"+e);
+    System.out.println("the division of two numbers"+f);
+  }
+}
