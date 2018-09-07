a14010bd682ed7c16067d58c1c06261c3756ea55
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-07 11:57:21 +0530

    Add files via upload

---
+++ intfloat calculation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   int a;float b;float c;float d;float e;float g;
+    a =15;b =5.5f;
+    c = a+b;
+    d = a-b;
+    e = a*b;
+    g = a/b;
+    System.out.println("the addition of two numbers"+c);
+    System.out.println("the subtraction of two numbers"+d);
+    System.out.println("the product of two numbers"+e);
+    System.out.println("the division of two numbers"+g);
+      
+  }
+}
