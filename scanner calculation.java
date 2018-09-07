21bdc36e3ddf60bff834ed9e44cf200a40e4b8cd
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-07 13:55:29 +0530

    Add files via upload

---
+++ scanner calculation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+   int a,b,c,d,e,f,g,h,i,j;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter the first num");
+   a =sc.nextInt();
+   System.out.println("enter the sec num");
+   b =sc.nextInt();
+   System.out.println("enter the third num");
+   c =sc.nextInt();
+   System.out.println("enter the fourth num");
+   d =sc.nextInt();
+   System.out.println("enter the fifth num");
+   e =sc.nextInt();
+   System.out.println("enter the sixth num");
+   f =sc.nextInt();
+   System.out.println("enter the seventh num");
+   g =sc.nextInt();
+   System.out.println("enter the eighth num");
+   h =sc.nextInt();
+   i = a+b+c+d+e+f+g+h;
+   j = a-b-c-d-e-f-g-h;
+   System.out.println("addition of eight numbers"+i);
+   System.out.println("subtraction of eight numbers"+j);
+   }
+}
