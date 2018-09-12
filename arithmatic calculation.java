a266235493ed6f669169d2430f6143221e42a343
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-12 15:13:29 +0530

    Add files via upload

---
+++ arithmatic calculation.java
@@ -0,0 +1,34 @@
+class Main {
+  public static void main(String[] args) {
+  int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+  a = 10;b = 15;
+      c=a+b;
+      d=a-b;
+      e=a*b;
+      f=b/a;
+      g=b%a;
+      h=b++;
+      i=b--;
+      j=a&b;
+      k=a|b;
+      l=a^b;
+      m=~a;
+      n=a<<2;
+      o=a>>2;
+      p=a>>>2;
+      System.out.println("the output"+c);
+      System.out.println("the output"+d);
+      System.out.println("the output"+e);
+      System.out.println("the output"+f);
+      System.out.println("the output"+g);
+      System.out.println("the output"+h);
+      System.out.println("the output"+i);
+      System.out.println("the output"+j);
+      System.out.println("the output"+k);
+      System.out.println("the output"+l);
+      System.out.println("the output"+m);
+      System.out.println("the output"+n);
+      System.out.println("the output"+o);
+      System.out.println("the output"+p);
+     }
+}
