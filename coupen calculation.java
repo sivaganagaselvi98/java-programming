9e62a14fdd1eb4656523cbfa8a8c0730afb15878
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-14 15:20:44 +0530

    Add files via upload

---
+++ coupen calculation.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+  int amount = 5000;
+  if(amount<=5000)
+  System.out.println("Free BackPck");
+  else if(amount<=10000)
+  System.out.println("Free Travel Bag");
+  else if(amount<=20000)
+  System.out.println("Free Trolly Suitcase");
+   else if(amount>=5000)
+  System.out.println("Collect money even for cover we give");
+  }
+}
