c3335886c2e61fc92a8afe69ba39e079e018de05
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-09-17 15:06:06 +0530

    Add files via upload

---
+++ leap calculation.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int leapYear=1900;
+    while (leapYear <= 2020){
+    for(int x=0;x<10; x++) {
+    if (leapYear>2020)
+    System.out.print(leapYear + " ");
+    leapYear = leapYear + 4;
+    }
+   }
+}
+}
