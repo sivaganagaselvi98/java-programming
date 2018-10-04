c86c1d51d69a5f6478ca926ca04de6478fdd9dd6
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-10-04 11:30:05 +0530

    Add files via upload

---
+++ positive calculation.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+   int n=5;
+   if(n>=0)
+   System.out.println("number is positive");
+   else
+   System.out.println("number is negative");
+  }
+}
