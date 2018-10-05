c04d91702c1a90e6bb6aaaaac52eee49711f30ef
sivaganagaselvi98 <43056876+sivaganagaselvi98@users.noreply.github.com>
2018-10-05 12:00:59 +0530

    Update Largest calculation.java

--- Largest calculation.java
+++ Largest calculation.java
@@ -5,12 +5,12 @@ public class Largest {
         double n1 = -5.5, n2 = 1.6, n3 = 6.7;
 
         if( n2 >= n1 && n2 >= n3)
-            System.out.println(n1 + " is the largest number.");
+            System.out.println(n1 + " largest number.");
 
         else if (n1 >= n2 && n1 >= n3)
-            System.out.println(n2 + " is the largest number.");
+            System.out.println(n2 + "largest number.");
 
         else
-            System.out.println(n3 + " is the largest number.");
+            System.out.println(n3 + "largest number.");
     }
 }
